22eeea6534896f08a0e6c513f30b90987617457a
Mike Frampton <mikeframpo@gmail.com>
2014-05-25 19:34:55 +1200

    Renamed main to game. Image is resized to fit container.

--- game.py
+++ game.py
@@ -29,10 +29,11 @@ class BingImageFetcher:
         keyfile = open(keypath, 'r')
         key = keyfile.readline().strip()
         self.bing = BingSearchAPI(key)
-        self.params = {'ImageFilters':'"Face:Face"',
-                  '$format': 'json',
-                  '$top': 10,
-                  '$skip': 0}
+        self.params = {
+                        #'ImageFilters':'"Face:Face"',
+                        '$format': 'json',
+                        '$top': 10,
+                        '$skip': 0}
 
     TIMEOUT = 10.0
     IMG_FILES = 'img'
@@ -44,15 +45,18 @@ class BingImageFetcher:
         if len(image_results) == 0:
             raise Exception('Failed to find any images for query ' + word)
         image_url = image_results[0]['MediaUrl']
-        req_image = requests.get(image_url, timeout=BingImageFetcher.TIMEOUT)
-        if req_image.status_code != 200:
-            raise Exception('Image request for %s failed to return 200 status'
-                            % image_url)
         up = urlparse.urlparse(image_url)
         destfile = os.path.basename(up.path)
         destpath = os.path.join(BingImageFetcher.IMG_FILES, destfile)
         if not os.path.isdir(BingImageFetcher.IMG_FILES):
             os.mkdir(BingImageFetcher.IMG_FILES)
+        if os.path.isfile(destpath):
+            # if we already have that image then just use the cached version
+            return destpath
+        req_image = requests.get(image_url, timeout=BingImageFetcher.TIMEOUT)
+        if req_image.status_code != 200:
+            raise Exception('Image request for %s failed to return 200 status'
+                            % image_url)
         imgfile = open(destpath, 'w')
         imgfile.write(req_image.content)
         imgfile.close()
@@ -60,18 +64,25 @@ class BingImageFetcher:
 
 class Game:
 
-    def __init__(self, screen, path):
+    def __init__(self):
         self.prev_draw_time = None
-        self.screen = screen
-        self.model = ModelIter(path)
+        self.screen = pg.display.set_mode(Game.SCREEN_DIMS)
+ 
+        self.model = None
         self.font = pg.font.SysFont('monospace', Game.TEXT_SIZE)
 
         # initial state of the game
         self.state = Game.STATE_NEWGAME
         self.draw_newgame_screen()
 
+        self.fetcher = BingImageFetcher(Game.KEYPATH)
+
+    SCREEN_DIMS = Vec2d(960, 720)
+    IMG_DIMS = Vec2d(SCREEN_DIMS.x * 0.8, SCREEN_DIMS.y * 0.7)
+
     STATE_NEWGAME = 0
     STATE_DRAWING = 1
+    STATE_LOADING = 2
 
     IMAGE_RATE = 20
     IMAGE_RATE_MILLIS = 1.0/IMAGE_RATE * 1000 
@@ -89,6 +100,8 @@ class Game:
     P1_CONTROLS_POS = Vec2d(50, 100)
     P2_CONTROLS = (pg.K_i, pg.K_o, pg.K_k, pg.K_l)
     P2_CONTROLS_POS = Vec2d(400, 100)
+
+    KEYPATH = 'key.txt'
 
     def draw_text(self, text, loc):
         text_surface = self.font.render(text, Game.TEXT_ANTIALIAS,
@@ -125,26 +138,66 @@ class Game:
                     3)
         self.screen.blit(controls_surface, loc)
 
+    def draw_loading_screen(self):
+        self.screen.fill(self.BG_COLOR)
+        loading_text = self.font.render('Loading next image...',
+                                        Game.TEXT_ANTIALIAS, Game.TEXT_COLOR)
+        self.screen.blit(loading_text, (0,0))
+        pg.display.flip()
+
+    def draw_step_drawing_screen(self):
+        # draw the words on the screen
+        im = self.model.step()
+        im_size = Vec2d(im.size)
+        r_x = float(im_size.x)/Game.IMG_DIMS.x
+        r_y = float(im_size.y)/Game.IMG_DIMS.y
+
+        if r_x > r_y:
+            im_newsize = im_size/r_x
+        else:
+            im_newsize = im_size/r_y
+        im_newsize = Vec2d(int(im_newsize.x), int(im_newsize.y))
+        im_resized = im.resize(im_newsize)
+
+        surface = pg.image.fromstring(im_resized.tostring(), im_resized.size,
+                                    im_resized.mode)
+        im_loc = Vec2d(Game.SCREEN_DIMS.x/2 - im_newsize.x/2, 30)
+        self.screen.blit(surface, im_loc)
+        pg.display.flip()
+
     def process_state(self, time, events):
         nextstate = None
+
         # process the current state
         if self.state == Game.STATE_NEWGAME:
             for event in events:
                 if event.type == pg.KEYUP and event.key == self.KEY_NEWGAME:
-                    nextstate = Game.STATE_DRAWING
+                    nextstate = Game.STATE_LOADING
+        elif self.state == Game.STATE_LOADING:
+            if self.model is None:
+                print('Image fetch failed')
+                nextstate = Game.STATE_LOADING
+            else:
+                nextstate = Game.STATE_DRAWING
         elif self.state == Game.STATE_DRAWING:
             assert self.prev_draw_time is None or time > self.prev_draw_time
             if self.prev_draw_time is None \
                 or (time - self.prev_draw_time) > Game.IMAGE_RATE_MILLIS:
                 self.prev_draw_time = time
-                im = self.model.step()
-                surface = pg.image.fromstring(im.tostring(), im.size, im.mode)
-                self.screen.blit(surface, (0,0))
-                pg.display.flip()
+                self.draw_step_drawing_screen()
+
         # enter the next state
         if nextstate is not None:
             if nextstate == Game.STATE_NEWGAME:
                 self.draw_newgame_screen()
+            if nextstate == Game.STATE_LOADING:
+                self.draw_loading_screen()
+                # TODO: pull a random word out of the wordlist
+                self.model = None
+                # TODO: catch the exception for a failed fetch
+                imagepath = self.fetcher.fetch_image('flower')
+                self.model = ModelIter(imagepath)
+                
             # set the new state
             self.state = nextstate
 
@@ -152,11 +205,10 @@ class Main:
 
     FRAMERATE = 60
 
-    def __init__(self, path):
+    def __init__(self):
         pg.init()
         self.clock = pg.time.Clock()
-        screen = pg.display.set_mode((1280, 960))
-        self.game = Game(screen, path)
+        self.game = Game()
 
     def mainloop(self):
         while True:
@@ -175,11 +227,6 @@ def test_bing_search(word):
     return(bing.fetch_image(word))
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    if len(args) != 1:
-        print 'Usage: python main.py input_image'
-        sys.exit(1)
-    
-    main = Main(args[0])
-    main.mainloop()
+
+    test_bing_search('jazzman')
 
